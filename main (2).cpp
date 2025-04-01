c715bc6c0d443fa3c2d5f4411b1f66ebb9bd5eba
Mihretab <mihretab43700@gmail.com>
2025-04-01 08:17:17 -0900

    Add files via upload

---
+++ main (2).cpp
@@ -0,0 +1,87 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+const int Max_users = 5;
+
+struct User {
+    string name;
+    string email;
+};
+
+struct Event {
+    string event_name;
+    int event_date;
+    string event_location;
+};
+
+struct Ticket {
+    int ticket_id;
+    User users[Max_users];
+    Event event;
+    int seat_numbers[Max_users];
+    int user_count;
+    bool is_vip;
+
+    Ticket* next;
+};
+
+class TicketSystemLinkedList {
+private:
+    Ticket* head;
+
+public:
+    void update_ticket() {
+        int id;
+        cout << "Enter Ticket ID to update: ";
+        cin >> id;
+
+        Ticket* temp = head;
+        while (temp) {
+            if (temp->ticket_id == id) {
+                int user_index;
+                cout << "Enter user index to update seat number (1 to 5 " << temp->user_count << "): ";
+                cin >> user_index;
+                cin.ignore();
+                user_index--;
+
+                if (user_index < 0 || user_index >= temp->user_count) {
+                    cout << "Invalid user index." << endl;
+                    return;
+                }
+
+                cout << "Enter new seat number for User " << (user_index + 1) << ": ";
+                cin >> temp->seat_numbers[user_index];
+                cin.ignore();
+                cout << "Ticket " << id << " updated successfully." << endl;
+                return;
+            }
+            temp = temp->next;
+        }
+        cout << "Ticket not found." << endl;
+    }
+   };
+
+int main() {
+    TicketSystemLinkedList system;
+    int choice;
+
+    while (true) {
+        cout << "\n--- Event Ticketing & Seat Allocation System ---" << endl;
+        cout << "1. Update Ticket" << endl;
+        cout << "2. Exit" << endl;
+        cout << "Enter your choice: ";
+        cin >> choice;
+
+        switch (choice) {
+            case 1:
+                system.update_ticket();
+                break;
+            case 2:
+                cout << "Exiting..." << endl;
+                return 0;
+            default:
+                cout << "Invalid choice. Please try again." << endl;
+        }
+    }
+}
